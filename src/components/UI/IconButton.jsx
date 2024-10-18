af57c911d43ef40817eb6ac57b4efbb98204ee24
shubham kumari <kumarishubham273@gmail.com>
2024-10-18 13:09:45 +0530

    keys

---
+++ src/components/UI/IconButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { log } from '../../log.js';
+
+export default function IconButton({ children, icon, ...props }) {
+  log('<IconButton /> rendered', 2);
+
+  const Icon = icon;
+  return (
+    <button {...props} className="button">
+      <Icon className="button-icon" />
+      <span className="button-text">{children}</span>
+    </button>
+  );
+}
